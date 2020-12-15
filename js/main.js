4ba0756948e34257cb5801e9b6dafcd1e8b71b92
James Huynh <mrpaperman@live.ca>
2020-12-14 20:03:17 -0500

    Linked with status server endpoint; populates table

--- js/main.js
+++ js/main.js
@@ -1,19 +1,56 @@
 "use strict";
 
-function main() {
-    console.log("!");
-    var url = "https://status.cabbagecanfly.com/api/v1/mc/vanilla"
+var statusEndpoint = "https://status.cabbagecanfly.com/api/v1/"
+var serverMap = {
+    "mc.cabbagecanfly.com": "mc/vanilla",
+    "ftb.cabbagecanfly.com": "mc/ftb",
+    "dummy.cabbagecanfly.com": "mc/dummy"
+}
+
+function updateTableRow(tableRow, content) {
+    if (content.online) {
+        tableRow.className += " online";
+        tableRow.querySelector(".server-icon img").src = content.icon;
+        tableRow.querySelector(".motd").textContent = content.motd;
+        tableRow.querySelector(".version").textContent = content.version;
+        tableRow.querySelector(".players").textContent =
+            content.players.online + "/" + content.players.max;
+    } else {
+        tableRow.className += " offline";
+        tableRow.querySelector(".motd").textContent = "Server offline.";
+    }
+}
+
+function getStatus(tableRow) {
+    var address = tableRow.querySelector(".address").textContent.trim();
+    var url = statusEndpoint + serverMap[address];
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (this.readyState == 4) {
             if (this.status == 200) {
                 var res = JSON.parse(xhr.responseText);
-                console.log(res);
+                updateTableRow(tableRow, res);
+            } else {
+                updateTableRow(tableRow, {});
             }
         }
     }
     xhr.send();
+}
+
+function getStatuses() {
+    // var addresses = document.querySelector(".mc-status-table").querySelectorAll(".address");
+    var tableRows = document.querySelector(".mc-status-table").querySelector("tbody").querySelectorAll("tr");
+    tableRows.forEach(function(e) {
+        // getStatus(e.textContent.trim());
+        getStatus(e);
+        console.log();
+    });
+}
+
+function main() {
+    getStatuses();
 }
 
 window.addEventListener("DOMContentLoaded", main, false);
